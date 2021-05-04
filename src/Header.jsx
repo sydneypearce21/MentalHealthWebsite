95c4a140596904f92ba7fa3ca89673fcdf5ce889
sambish5 <48037840+sambish5@users.noreply.github.com>
2021-05-04 15:31:21 -0400

    header update (header includes logo)

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import { Link } from 'react-router-dom' ;
 import { Container } from 'reactstrap';
-
+import  MindfulMentors  from './Images/MindfulMentors.jpg';
 import {
   Collapse,
   Navbar,
@@ -18,41 +18,41 @@ import {
 } from 'reactstrap';
 
 const Header = (props) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => setIsOpen(!isOpen);
-
-  return (
-      <Navbar color="light" light expand="md">
-        <Container>
-            <NavbarBrand href="/">Mindful Mentors</NavbarBrand>
-            <NavbarToggler onClick={toggle} />
-            <Collapse isOpen={isOpen} navbar>
-            <Nav className="mr-auto" navbar>
-                <NavItem><Link className="nav-link" to="/" activeClassName="active">Home</Link></NavItem>
-                <NavItem><Link className="nav-link" to="/learn" activeClassName="active">Learn</Link></NavItem>
-                <NavItem><Link className="nav-link" to="/activities" activeClassName="active">Activities </Link></NavItem>
-                <NavItem><Link className="nav-link" to="/resources" activeClassName="active">Resources</Link></NavItem>
-                <UncontrolledDropdown nav inNavbar>
-                    <DropdownToggle nav caret>
-                        Account
-                    </DropdownToggle>
-                    <DropdownMenu right>
-                        <DropdownItem>
-                            <Link className="nav-link" to="/login" activeClassName="active">Log In</Link>
-                        </DropdownItem>
-                        <DropdownItem>
-                            <Link className="nav-link" to="/register" activeClassName="active">Register</Link>
-                        </DropdownItem>
-                    </DropdownMenu>
-                </UncontrolledDropdown>
-            
-            </Nav>
-            </Collapse>
-        </Container>
-      </Navbar>
-    
-  );
-}
-
-export default Header;
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const toggle = () => setIsOpen(!isOpen);
+  
+    return (
+        <Navbar color="light" light expand="md">
+          <Container>
+              <NavbarBrand href="/"><img src={MindfulMentors} alt="MindfulMentors"/></NavbarBrand>
+              <NavbarToggler onClick={toggle} />
+              <Collapse isOpen={isOpen} navbar>
+              <Nav className="mr-auto" navbar>
+                  <NavItem><Link className="nav-link" to="/" activeClassName="active">Home</Link></NavItem>
+                  <NavItem><Link className="nav-link" to="/learn" activeClassName="active">Learn</Link></NavItem>
+                  <NavItem><Link className="nav-link" to="/activities" activeClassName="active">Activities </Link></NavItem>
+                  <NavItem><Link className="nav-link" to="/resources" activeClassName="active">Resources</Link></NavItem>
+                  <UncontrolledDropdown nav inNavbar>
+                      <DropdownToggle nav caret>
+                          Account
+                      </DropdownToggle>
+                      <DropdownMenu right>
+                          <DropdownItem>
+                              <Link className="nav-link" to="/login" activeClassName="active">Log In</Link>
+                          </DropdownItem>
+                          <DropdownItem>
+                              <Link className="nav-link" to="/register" activeClassName="active">Register</Link>
+                          </DropdownItem>
+                      </DropdownMenu>
+                  </UncontrolledDropdown>
+              
+              </Nav>
+              </Collapse>
+          </Container>
+        </Navbar>
+      
+    );
+  }
+  
+  export default Header;
